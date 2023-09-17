5db4bb7685cc904d6f1deef63e2e343ae53c796b
yeison <ycuervob@unal.edu.co>
2023-09-17 16:32:05 -0500

    Cambio estructura del agente se separan archivos y caracteristicas del ambiente

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,47 +1,4 @@
-import heapq
-
-class Node:
-    def __init__(self, state, parent=None, action=None, cost=0, heuristic=0):
-        self.state = state
-        self.parent = parent
-        self.action = action
-        self.cost = cost
-        self.heuristic = heuristic
-
-    def __lt__(self, other):
-        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
-
-def astar_search(initial_state, goal_state, actions_fn, heuristic_fn):
-    open_list = []
-    closed_set = set()
-
-    initial_node = Node(initial_state, cost=0, heuristic=heuristic_fn(initial_state))
-    heapq.heappush(open_list, initial_node)
-
-    while open_list:
-        current_node = heapq.heappop(open_list)
-
-        if current_node.state == goal_state:
-            path = []
-            while current_node:
-                path.append(current_node.action)
-                current_node = current_node.parent
-            return path[::-1]
-
-        if current_node.state in closed_set:
-            continue
-
-        closed_set.add(current_node.state)
-
-        for action in actions_fn(current_node.state):
-            next_state = action.execute(current_node.state)
-            if next_state not in closed_set:
-                cost = current_node.cost + action.cost
-                heuristic = heuristic_fn(next_state)
-                next_node = Node(next_state, parent=current_node, action=action, cost=cost, heuristic=heuristic)
-                heapq.heappush(open_list, next_node)
-
-    return None
+from Functions.astar_search import astar_search
 
 # Define your Agent class
 class Agent:
---
+++ Agent/Structures/Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, state, parent=None, action=None, cost=0, heuristic=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = cost
+        self.heuristic = heuristic
+
+    def __lt__(self, other):
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
