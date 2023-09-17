108663ed74159d10cfa6ea33df5ca40ad14bdaaa
yeison <ycuervob@unal.edu.co>
2023-09-17 18:33:33 -0500

    Corrección nombres

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -9,15 +9,18 @@ class Agent:
     def __init__(self, initial_state : np.ndarray):
         self.initial_state = initial_state
 
-     # Define las acciones posibles dado una matriz de dulces
+    # Define las acciones posibles dado una matriz de dulces
     def actions(self, state : np.ndarray, matrixCandy : np.ndarray):
         pass
 
     # Define una heuristica o funcion que maximize el resultado
-    def heuristic(self, state):
+    # Esta función recibe una matriz de dulces y devuelve un valor con el consto y las posiciones de los dulces a mover para el máximo
+    # de esa sola matriz
+    def matrixValue(self, matrixCandy : np.ndarray):
         pass
 
-    def solve(self):
+    # Devuelve el movimiento que debe realizar el agente pos x e y
+    def compute(self, perception : np.ndarray):
         return astar_search(self.initial_state, self.goal_state, self.actions, self.heuristic)
 
 # Example usage
