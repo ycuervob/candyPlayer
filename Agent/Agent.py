ef67f85f2f32a158489b7ce45eeebe3f613d302b
yeison <ycuervob@unal.edu.co>
2023-09-17 19:32:40 -0500

    Correccion tipos

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -11,16 +11,16 @@ class Agent:
 
     # Define las acciones posibles dado una matriz de dulces
     # Devuelve una lista de tuplas con: (punto inicial, punto final, costo, matriz resultante)
-    def actions(self, matrixCandy : np.ndarray) -> list[((int, int), (int, int), int, np.ndarray)]:
+    def actions(self, matrixCandy : np.ndarray) -> list[((np.int8, np.int8), (np.int8, np.int8), np.int8, np.ndarray)]:
         pass
 
     # Esta función recibe una matriz de dulces
     # Devuelve una lista de tuplas con: (punto inicial, punto final, costo)
-    def matrixValue(self, matrixCandy : np.ndarray) -> [(int, int), (int, int), int]:
+    def matrixValue(self, matrixCandy : np.ndarray, punto1 : (np.int8, np.int8), punto2 : (np.int8, np.int8)) -> [(np.int8, np.int8), (np.int8, np.int8), np.int8]:
         pass
 
     # Devuelve una tupla de tuplas con: (punto inicial, punto final, costo)
-    def compute(self, perception : str, matrixCandy : np.ndarray = None) -> ((int, int), (int, int), int):
+    def compute(self, perception : str, matrixCandy : np.ndarray = None) -> ((np.int8, np.int8), (np.int8, np.int8), np.int8):
         if(perception == "a"):
             return self.actions(matrixCandy)
         elif(perception == "v"):
@@ -31,8 +31,8 @@ class Agent:
         else:
             return None
 
-# Example usage
 
+# Example usage
 if(__name__ == "__main__"):
     agenteTest = Agent(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])) # Esto es solo un ejemplo esta no es la matriz real
     print("Solution:", agenteTest)
