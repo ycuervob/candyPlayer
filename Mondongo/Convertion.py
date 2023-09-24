bfad337e9d16ca5a5f414cd2a4897cf26496c289
yeison <ycuervob@unal.edu.co>
2023-09-23 20:28:00 -0500

    correccion

--- Mondongo/Convertion.py
+++ Mondongo/Convertion.py
@@ -1,8 +1,9 @@
 from .ScreenCapture import ScreenCapture
 #Clase que convierte la imagen al un arreglo de 9x9 de numpy
 class Convertion:
+    
+    def __init__(self, screenCapture: ScreenCapture):
+        self.screenCapture = screenCapture
 
-    class Convertion:
-        def __init__(self, screenCapture: ScreenCapture):
-            self.screenCapture = screenCapture
+
         
