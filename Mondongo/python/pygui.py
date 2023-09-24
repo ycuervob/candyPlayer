b44007bf5e77a9cb729c698e8fe9a72fe28f572b
yeison <ycuervob@unal.edu.co>
2023-09-23 19:09:25 -0500

    Clase de mover osi osi

--- Mondongo/python/pygui.py
+++ Mondongo/python/pygui.py
@@ -1,11 +1,37 @@
 import pyautogui
 
-# Mover el mouse a las coordenadas (x, y)
-pyautogui.moveTo(1920, 0)
+#Clase pointer para mover el mouse
+class pointer:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.pos = (self.x, self.y)
+        
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+        self.pos = (self.x, self.y)
+        pyautogui.moveTo(self.pos)
+        
+    def click(self):
+        pyautogui.click(self.pos)
+    
+    def twoClickMove(self, x, y, x2, y2):
+        self.move(x, y)
+        self.click()
+        self.move(x2, y2)
+        self.click()
 
-# Hacer clic en las coordenadas actuales del mouse
-pyautogui.click()
+    def getPos(self):
+        return self.pos
+    
 
-# Obtener las coordenadas actuales del mouse
-x, y = pyautogui.position()
-print(f"Coordenadas actuales del mouse: ({x}, {y})")
+#Test de class
+if __name__ == "__main__":
+    p = pointer()
+    p.move(100, 100)
+    p.click()
+    p.move(200, 200)
+    p.click()
+    p.twoClickMove(300, 300, 400, 400)
+    print(p.getPos())
