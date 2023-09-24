35bf9fbbd46f84592ce5dc5583afc5c975b13b49
yeison <ycuervob@unal.edu.co>
2023-09-24 16:22:04 -0500

    SUPER TRADUCCION A MATRIZ

--- Mondongo/Convertion.py
+++ Mondongo/Convertion.py
@@ -19,7 +19,7 @@ def mostrar_color_rgb(color_rgb):
 #Clase que convierte la imagen al un arreglo de 9x9 de numpy
 class Convertion:
 
-    def __init__(self, screenCapture: ScreenCapture, baseRoute =  "Mondongo/imgCandy/"):
+    def __init__(self, screenCapture: ScreenCapture, baseRoute =  "Mondongo/imgCandy/", umbral = 0.95):
         self.screenCapture = screenCapture
         self.colors = {
             0x01: (255, 255, 0),    #amarillo
@@ -29,13 +29,15 @@ class Convertion:
             0x05: (255, 0, 255),    #morado
             0x06: (255, 0, 0)       #rojo
         }
+        self.umbral = umbral
         self.arraytypes = np.array(["", "E", "E1", "E2"])
-        self.arrayAmarillos = [cv2.imread(baseRoute + "amarillo/amarillo"+ types +".png") for types in self.arraytypes]
-        self.arrayNaranjas = [cv2.imread(baseRoute + "naranja/naranja"+ types +".png") for types in self.arraytypes]
-        self.arrayAzules = [cv2.imread(baseRoute + "azul/azul"+ types +".png") for types in self.arraytypes]
-        self.arrayVerdes = [cv2.imread(baseRoute + "verde/verde"+ types +".png") for types in self.arraytypes]
-        self.arrayMorados = [cv2.imread(baseRoute + "morado/morado"+ types +".png") for types in self.arraytypes]
-        self.arrayRojos = [cv2.imread(baseRoute + "rojo/rojo"+ types +".png") for types in self.arraytypes]
+        self.arrayAmarillos = [cv2.cvtColor(cv2.imread(baseRoute + "amarillo/amarillo"+ types +".png") , cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayNaranjas = [cv2.cvtColor(cv2.imread(baseRoute + "naranja/naranja"+ types +".png"), cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayAzules = [cv2.cvtColor(cv2.imread(baseRoute + "azul/azul"+ types +".png"), cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayVerdes = [cv2.cvtColor(cv2.imread(baseRoute + "verde/verde"+ types +".png"), cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayMorados = [cv2.cvtColor(cv2.imread(baseRoute + "morado/morado"+ types +".png"), cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayRojos = [cv2.cvtColor(cv2.imread(baseRoute + "rojo/rojo"+ types +".png"), cv2.COLOR_BGR2GRAY) for types in self.arraytypes]
+        self.arrayCandy = [self.arrayAmarillos, self.arrayNaranjas, self.arrayAzules, self.arrayVerdes, self.arrayMorados, self.arrayRojos]
 
     def clasificarColorBasic(self, pixel):
         distancia_minima = float('inf')
@@ -83,30 +85,54 @@ class Convertion:
         for k in range(3):
             representativeColor[0][3][k] = image[divisionLength // 2][(3 * divisionWidth + divisionWidth // 2)-10][k]
 
-        #Clasificamos los colors
+        #Clasificamos los colores basicamente
         for i in range(9):
             for j in range(9):
                 representationArray[i][j] = self.clasificarColorBasic(representativeColor[i][j])
-    
-        
+
+        #Clasificamos los colores de los dulces mas especificamente usando openCV
+        for i in range(9):
+            for j in range(9):
+                candysOfSameColor = self.arrayCandy[representationArray[i][j]-1]
+                for candy in candysOfSameColor:
+                    histCandy = cv2.calcHist([candy], [0], None, [256], [0, 256])
+                    histImage = cv2.calcHist([image[i * divisionLength: (i + 1) * divisionLength, j * divisionWidth: (j + 1) * divisionWidth]], [0], None, [256], [0, 256])
+                    simliarity = cv2.compareHist(histCandy, histImage, cv2.HISTCMP_CORREL)
+                    if simliarity > self.umbral and candysOfSameColor.index(candy) > 0:
+                        representationArray[i][j] += 9
+                        break
+                    
+
         return representationArray
     
 async def tarea1():
-    # asyncio.create_subprocess_exec("make", "start")
-    # await asyncio.sleep(5)
-    # webbrowser.open("http://127.0.0.1:3006")
-    # await asyncio.sleep(2)
-    # pointer = Pointer()
-    # pointer.moveAndClick(750, 350)
-    # await asyncio.sleep(30)
+    asyncio.create_subprocess_exec("make", "start")
+    await asyncio.sleep(5)
+    webbrowser.open("http://127.0.0.1:3006")
+    await asyncio.sleep(3)
+    pointer = Pointer()
+    pointer.moveAndClick(750, 350)
+    await asyncio.sleep(30)
     sc = ScreenCapture()
-    c = Convertion(sc)
+    c = Convertion(sc, umbral=0.9)
     array = c.convert()
     print(array)
     print(array.shape)
-    # for i in range(9):
-    #     for j in range(9):
-    #         mostrar_color_rgb(tuple(x/255 for x in array[i][j]))
+    for i in array:
+        for j in i:
+            if j == 1:
+                print("Amarillo", end=" ")
+            elif j == 2:
+                print("Naranja", end=" ")
+            elif j == 3:
+                print("Azul", end=" ")
+            elif j == 4:
+                print("Verde", end=" ")
+            elif j == 5:
+                print("Morado", end=" ")
+            elif j == 6:
+                print("Rojo", end=" ")
+        print()
 
 if __name__ == "__main__":
     asyncio.run(tarea1())
