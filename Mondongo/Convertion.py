45902644fe5c1ce904a1a98320f53f8b7dd7e56d
yeison <ycuervob@unal.edu.co>
2023-09-24 00:16:50 -0500

    Clase que convierte imagen a matriz y matriz a puntos para el mouse

--- Mondongo/Convertion.py
+++ Mondongo/Convertion.py
@@ -1,9 +1,24 @@
-from .ScreenCapture import ScreenCapture
+from ScreenCapture import ScreenCapture
+import numpy as np
 #Clase que convierte la imagen al un arreglo de 9x9 de numpy
 class Convertion:
     
     def __init__(self, screenCapture: ScreenCapture):
         self.screenCapture = screenCapture
 
+    def convert(self, division = 9):
+        #Obtenemos la imagen
+        image = self.screenCapture.getScreen()
+        #Convertimos la imagen a un arreglo de numpy
+        image = np.array(image)
 
+        #Convertimos la imagen a un arreglo de 9x9
+        imageLength, imagewidth = image.shape
         
+        print(imageLength)
+        
+    
+if __name__ == "__main__":
+    sc = ScreenCapture()
+    c = Convertion(sc)
+    c.convert()
