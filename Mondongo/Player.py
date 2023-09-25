7e18289cfb50b8bf8bdea87f29870c9f48b96d70
yeison <ycuervob@unal.edu.co>
2023-09-24 19:19:56 -0500

    otro

--- Mondongo/Player.py
+++ Mondongo/Player.py
@@ -21,8 +21,10 @@ class Player:
         y = 10
         width = 650
         height = 580
+        scuareWidth = width/9
+        scuareHeight = height/9
 
-        xPixelI, yPixelI = inicial[0] * (width/9) + x , inicial[1] * (height/9) + y
-        xPixelF, yPixelF = final[0] * (width/9) + x , final[1] * (height/9) + y
+        xPixelI, yPixelI = inicial[0] * scuareWidth + x + scuareWidth/2 , inicial[1] * scuareHeight + y + scuareHeight/2
+        xPixelF, yPixelF = final[0] * scuareWidth + x + scuareWidth/2 , final[1] * (height/9) + y + scuareHeight/2
 
         self.pointer.twoClickMove(xPixelI, yPixelI, xPixelF, yPixelF)
--- enviroment.py
+++ enviroment.py
@@ -3,6 +3,7 @@ from Mondongo.Convertion import Convertion
 from Mondongo.Player import Player
 from Mondongo.Pointer import Pointer
 from Mondongo.ScreenCapture import ScreenCapture
+import keyboard
 
 import webbrowser
 import asyncio
@@ -29,47 +30,10 @@ async def play():
     await init()
     global c, a, p
 
-    while True:
-        #conseguir acciones desde la pantalla
-        acciones = a.actions(c.convert())
-        print(acciones)
-
-        #top 5 acciones en O(n + k log k)
-        top_indices = np.argpartition(acciones[:, -1], -5)[-5:]
-
-        mejoresAcciones = acciones[top_indices]
-
-        #euristica
-
-        #TODO paralelizarlo
-        a1 = Agent(np.array([], dtype=np.int8))
-        a2 = Agent(np.array([], dtype=np.int8))
-        a3 = Agent(np.array([], dtype=np.int8))
-        a4 = Agent(np.array([], dtype=np.int8))
-        a5 = Agent(np.array([], dtype=np.int8))
-
-        euristica = [
-            a1.compute("s",mejoresAcciones[0])[2],
-            a2.compute("s",mejoresAcciones[1])[2],
-            a3.compute("s",mejoresAcciones[2])[2],
-            a4.compute("s",mejoresAcciones[3])[2],
-            a5.compute("s",mejoresAcciones[4])[2],
-            ]
-        
-        # mejor movimiento de acuerdo a la euristica
-        arr1 = np.array([
-            mejoresAcciones[0][2],
-            mejoresAcciones[1][2],
-            mejoresAcciones[2][2],
-            mejoresAcciones[3][2],
-            mejoresAcciones[4][2],
-        ])
-
-        arr2 = np.array(euristica)
-        resultado = arr1 + arr2
-
-        mejorMovimiento = np.argmax(resultado)
-
-        p.movimiento(mejoresAcciones[mejorMovimiento])
+    #conseguir acciones desde la pantalla
+    sol = a.compute("s", c.convert())
+    print(sol)
+    #ejecutar acciones
+    p.movimiento(sol)
 
 asyncio.run(play())
