1c586897b3ab6c25039bf82bf48aa6144f7d2174
dvillamor <dvillamor@unal.edu.co>
2023-09-24 20:24:52 -0500

    enviroment mamadisimo

--- enviroment.py
+++ enviroment.py
@@ -1,39 +1,85 @@
-from Agent.Agent import Agent
 from Mondongo.Convertion import Convertion
 from Mondongo.Player import Player
-from Mondongo.Pointer import Pointer
 from Mondongo.ScreenCapture import ScreenCapture
-import keyboard
+from Mondongo.Pointer import Pointer
+from Agent.Agent import Agent
 
 import webbrowser
 import asyncio
 import numpy as np
 
-c = None
-a = None
-p = None
-
 async def init():
-    global c, a, p
-    await asyncio.create_subprocess_exec("http-server", "Game", "-p", "3006")
+    asyncio.create_subprocess_exec("make", "start")
+    await asyncio.sleep(5)
     webbrowser.open("http://127.0.0.1:3006")
-    await asyncio.sleep(2)
+    await asyncio.sleep(5)
     pointer = Pointer()
     pointer.moveAndClick(750, 350)
-    await asyncio.sleep(26)
+    await asyncio.sleep(30)
     sc = ScreenCapture()
     c = Convertion(sc, umbral=0.9)
     a = Agent(np.array([], dtype=np.int8))
     p = Player(pointer)
 
+    return c,a,p
+
 async def play():
-    await init()
-    global c, a, p
+    c,a,p = await init()
 
-    #conseguir acciones desde la pantalla
-    sol = a.compute("s", c.convert())
-    print(sol)
-    #ejecutar acciones
-    p.movimiento(sol)
+    i = 0
+    while i < 2:
+        #conseguir acciones desde la pantalla
+        matriz = c.convert()
+        acciones = a.actions(matriz)
+        acciones = np.array(acciones, dtype=object)
 
-asyncio.run(play())
+        #top 5 acciones en O(n + k log k)
+        top_indices = np.argpartition(acciones[:, -1], -5)[-5:]
+
+        mejoresAcciones = acciones[top_indices]
+
+        #euristica
+
+        #TODO paralelizarlo
+        a1 = Agent(np.array([], dtype=np.int8))
+        a2 = Agent(np.array([], dtype=np.int8))
+        a3 = Agent(np.array([], dtype=np.int8))
+        a4 = Agent(np.array([], dtype=np.int8))
+        a5 = Agent(np.array([], dtype=np.int8))
+
+        matriz1 = a1.matrixValue(matriz,mejoresAcciones[0][0],mejoresAcciones[0][1])[1]
+        matriz2 = a2.matrixValue(matriz,mejoresAcciones[1][0],mejoresAcciones[1][1])[1]
+        matriz3 = a3.matrixValue(matriz,mejoresAcciones[2][0],mejoresAcciones[2][1])[1]
+        matriz4 = a4.matrixValue(matriz,mejoresAcciones[3][0],mejoresAcciones[3][1])[1]
+        matriz5 = a5.matrixValue(matriz,mejoresAcciones[4][0],mejoresAcciones[4][1])[1]
+
+        euristica = [
+            a1.compute("s",matriz1)[2],
+            a2.compute("s",matriz2)[2],
+            a3.compute("s",matriz3)[2],
+            a4.compute("s",matriz4)[2],
+            a5.compute("s",matriz5)[2],
+            ]
+        
+        # mejor movimiento de acuerdo a la euristica
+        arr1 = np.array([
+            mejoresAcciones[0][2],
+            mejoresAcciones[1][2],
+            mejoresAcciones[2][2],
+            mejoresAcciones[3][2],
+            mejoresAcciones[4][2],
+        ])
+
+        arr2 = np.array(euristica)
+        resultado = arr1 + arr2
+
+        mejorMovimiento = np.argmax(resultado)
+        print("mejorMovimiento:", mejoresAcciones[mejorMovimiento])
+        await p.movimiento(mejoresAcciones[mejorMovimiento])
+        i+=1
+
+async def main():
+    await play()
+
+if __name__ == "__main__":
+    asyncio.run(main())
