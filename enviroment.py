922edc1145ade2a7c82625027a9946169fdf5866
dvillamor <dvillamor@unal.edu.co>
2023-09-24 20:54:46 -0500

    evitando daños

--- enviroment.py
+++ enviroment.py
@@ -32,42 +32,24 @@ async def play():
         acciones = a.actions(matriz)
         acciones = np.array(acciones, dtype=object)
 
+        num_agentes = min(len(acciones), 5)
+
         #top 5 acciones en O(n + k log k)
-        top_indices = np.argpartition(acciones[:, -1], -5)[-5:]
+        top_indices = np.argpartition(acciones[:, -1], -5)[-num_agentes:]
 
         mejoresAcciones = acciones[top_indices]
 
         #euristica
 
         #TODO paralelizarlo
-        a1 = Agent(np.array([], dtype=np.int8))
-        a2 = Agent(np.array([], dtype=np.int8))
-        a3 = Agent(np.array([], dtype=np.int8))
-        a4 = Agent(np.array([], dtype=np.int8))
-        a5 = Agent(np.array([], dtype=np.int8))
+        agentes = [Agent(np.array([], dtype=np.int8)) for x in range(num_agentes)]
 
-        matriz1 = a1.matrixValue(matriz,mejoresAcciones[0][0],mejoresAcciones[0][1])[1]
-        matriz2 = a2.matrixValue(matriz,mejoresAcciones[1][0],mejoresAcciones[1][1])[1]
-        matriz3 = a3.matrixValue(matriz,mejoresAcciones[2][0],mejoresAcciones[2][1])[1]
-        matriz4 = a4.matrixValue(matriz,mejoresAcciones[3][0],mejoresAcciones[3][1])[1]
-        matriz5 = a5.matrixValue(matriz,mejoresAcciones[4][0],mejoresAcciones[4][1])[1]
+        matrices = [agentes[i].matrixValue(matriz,mejoresAcciones[i][0],mejoresAcciones[i][1])[1] for i in range(len(agentes))]
 
-        euristica = [
-            a1.compute("s",matriz1)[2],
-            a2.compute("s",matriz2)[2],
-            a3.compute("s",matriz3)[2],
-            a4.compute("s",matriz4)[2],
-            a5.compute("s",matriz5)[2],
-            ]
+        euristica = [agentes[i].compute("s",matrices[i])[2] for i in range(len(agentes))]
         
         # mejor movimiento de acuerdo a la euristica
-        arr1 = np.array([
-            mejoresAcciones[0][2],
-            mejoresAcciones[1][2],
-            mejoresAcciones[2][2],
-            mejoresAcciones[3][2],
-            mejoresAcciones[4][2],
-        ])
+        arr1 = np.array([mejoresAcciones[i][2] for i in range(len(agentes))])
 
         arr2 = np.array(euristica)
         resultado = arr1 + arr2
